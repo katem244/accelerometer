da64c01f008543435e533f22ff16587a25fae7dd
katem244 <kat@dhcp-ccc-5905.redrover.cornell.edu>
2016-06-27 13:57:34 -0400

    First commit

---
+++ accelerometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  accelerometer
+//
+//  Created by Katerina on 6/23/16.
+//  Copyright © 2016 Katerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
