13b804b66cdfa7f191870f904d15abb71e74b3ed
katem244 <kat@dhcp-ccc-5905.redrover.cornell.edu>
2016-06-29 13:38:58 -0400

    added touch

--- accelerometer/AppDelegate.h
+++ accelerometer/AppDelegate.h
@@ -8,10 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder
 
 @property (strong, nonatomic) UIWindow *window;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
-
